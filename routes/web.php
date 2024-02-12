e7499d6db48667d6bc31d40099ce598d672f437d
MohamedAbakrim <mohammedabakrim@gmail.com>
2024-02-12 23:39:36 +0300

    what I did is I created routes with parameters and I learned how to group routes and name them if u return a string in a route the server is going to return a html if u return an array its going to return a json I also worked with request to show diferent thigns like a specific parameter or the url or the path

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('/blog')->name('blog.')->group(function(){
+
+    Route::get('/', function(){
+        return [
+            "link"=> \route('blog.show', ["id" => "30", "title" => "harrypotter"])
+        ];
+    })->name('index');
+    
+    
+    Route::get('/{title}/{id}', function(string $title, string $id){
+        return [
+            "id" => $id,
+            "title"=>$title
+        ];
+    })->name('show');
+
+});
+
+
+
+Route::get('/test', function(Request $request){
+
+    /*
+        if u are just returing a string the server are going to response as an html content
+        if u returning an array its going to be json
+    */
+
+    return [
+        "parameters" => $request->all(), // this returns all the parameters in the url
+        "url" => $request->url(), // this returns the url of ur app
+        "path" => $request->path(), // this returns the current route
+        "name" => $request->input("name"), // this returns the value of the parameter name that passed in the url if its empty its gonna be null! and u can give it a default value if u want
+        "content" => "Hello World!"
+    ];
 });
